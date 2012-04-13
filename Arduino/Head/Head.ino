0e1a3eb07a97e08c2abd2c7b4e388b9287bc18ab
Iñigo Beitia <luckybeitia@gmail.com>
2012-04-13 11:27:14 -0400

    Added retool and serial handling (high level).

--- Arduino/Head/Head.ino
+++ Arduino/Head/Head.ino
@@ -1,49 +1,95 @@
-// Cam
-
 #include <Servo.h> 
  
 Servo headRotation;
 Servo headCam;
  
-int pos = 0;
-int pos2 = 0;
- 
-void setup() 
-{ 
+int rotationDegrees = 0;
+int camAngle = 0;
+int opID = 0;
+int messageInfo = 0;
+int x = 0;
+int y = 0;
+int z = 0;
+
+void setup() { 
   headRotation.attach(9);  
   headCam.attach(8);
   Serial.begin(9600);
 } 
  
  
-void loop() 
-{
-  //horizontal();
-  //pushDown();
-  headCam.write(0);
-  delay(1000);
-  headCam.write(10);
-  //vertical();
-  //goUp();
-  delay(1000);
+void loop() {
+  while (Serial.available() > 15) {
+    // Message ID
+    opID =  Serial.read(); 
+    
+    // Message
+    messageInfo = Serial.read();
+    
+    switch (opID) {
+      case 1: // Push down cam
+              pushDown();
+              delay(1000);
+              break;
+      case 2: // Move cam up
+              goUp();
+              delay(1000);
+              break;
+      case 3: // Rotate head horizontally
+              horizontal();
+              delay(1000);
+              break;
+      case 4: // Rotate head vertically
+              vertical();
+              delay(1000);
+              break;
+      case 5: // Move X
+              moveStepper(1, messageInfo);
+              delay(1000);
+              break;
+      case 6: // Move Y
+              moveStepper(2, messageInfo);
+              delay(1000);
+              break;
+      case 7: // Move Z
+              moveStepper(2, messageInfo);
+              delay(1000);
+              break;
+    }
+    
+    sendConfirmation();
+  }
+}
+
+void sendConfirmation() {
+  Serial.print(1);
+}
+
+void moveStepper(axis, amount) {
+  if (amount < 0) {
+    // Move backwards
+  } else if (amount > 0) {
+    // Move forwards
+   
+  } 
 }
 
 void horizontal() {
-  pos = 38;
-  headRotation.write(pos);   // 28 = 0º
+  rotationDegrees = 38;
+  headRotation.write(rotationDegrees);
 }
 
 void vertical() {
-  pos = 138;
-  headRotation.write(pos);   // 28 = 0º
+  rotationDegrees = 138;
+  headRotation.write(rotationDegrees);
 }
 
 void pushDown() {
-  pos2 = 32;
-  headCam.write(pos2);   // 28 = 0º
+  camAngle = 32;
+  headCam.write(camAngle);
 }
 
 void goUp() {
-  pos2 = 0;
-  headCam.write(pos2);   // 28 = 0º
+  camAngle = 0;
+  headCam.write(camAngle);
 }
