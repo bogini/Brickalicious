484d555be85994bd2e0668420968665d956b49d5
Larissa Little <larissalittle@gmail.com>
2012-04-13 10:38:24 -0400

    changed Interpreter.py and began to update Printer.py

--- Printing/Printer.py
+++ Printing/Printer.py
@@ -1,45 +1,54 @@
 CARTRIDGE_X = -1
 CARTRIDGE_Y = 0
 CARTRIDGE_Z = 0
-PICKUP_DISTANCE = 0.20
+PICKUP_DISTANCE = 0.16
 
 class Printer:
+	"""
+	"""
     def __init__(self, bricks):
         self.bricks = bricks
         self.bricks_to_print = self.bricks.copy()
         self.x = 0
         self.y = 0
         self.z = 0
-        self.printing = false
-        self.move_to_cartridge()
     
-    def print_bricks(self):
-        # Goes through the bruck list
+    def generate_command_list(self):
+		"""
+		creates a list of all the commands that need to be sent to the 
+		arduino in the right order
+		"""
+        self.set_head()#move over until we hit limit switches, set head into picking up position
         for brick in self.bricks:
-            # Check if we are paused
-            while(not self.printing):
-                pass
-            self.move_to_cartridge()
-            self.pick_up()
+            self.pick_up() 
             self.move(brick.pin.x, brick.pin.y, brick.pin.z + PICKUP_DISTANCE)
             self.lay_down()
             bricks_to_print.remove(brick)
-        self.move_to_cartridge()
+            self.move_to_cartridge()
     
     def move_to_cartridge(self):
+		"""
+		moves from current x,y,z
+		"""
         # Mobes the head to the cartridge
         self.move(CARTRIDGE_X, CARTRIDGE_Y, PICKUP_DISTANCE)
     
     def pick_up(self):
+		"""
+		"""
         # Picks up the piece
         move(self.x, self.y, CARTRIDGE_Z)
         
     def lay_down(self):
+		"""
+		"""
         # Lays down the piece
         move(self.x, self.y, PICKUP_DISTANCE)
         # Press down
         
     def move(self, to_X, to_Y, to_Z):
+		"""
+		"""
         # Moves the head to the specified coordinates
         # Move x
         # Move y
@@ -47,13 +56,3 @@ class Printer:
         self.x = to_X
         self.y = to_Y
         self.z = to_Z
-    
-    def pause(self):
-        # Pauses the printing
-        self.printing = not self.printing
-        self.bricks = self.bricks_to_print
-        
-    def cancel(self):
-        # Cancel the printing
-        self.printing = false
-        self.move_to_cartridge()
